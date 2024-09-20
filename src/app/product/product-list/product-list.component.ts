4853970a8666306c51ab1ea193e69433b9f98ed3
Paulina Itzel Aguilar Garcia <21000491@alumnos.utleon.edu.mx>
2024-09-20 17:28:03 -0600

    primer commit miercoles 18 de septiembre

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  productos:any[]=[
+    {
+      "productoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"4 puertas",
+      "Precio": 20000,
+      "Year":2023,
+      "Marca":"NISSAN",
+      "Color":"Azul",
+      "ImagenUrl":""
+    },
+    {
+      "productoId":2,
+      "Modelo":"Sentra",
+      "Descripcion":"4 puertas",
+      "Precio": 20000,
+      "Year":2023,
+      "Marca":"AUDI",
+      "Color":"Blanco",
+      "ImagenUrl":""
+    },
+    {
+      "productoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"4 puertas",
+      "Precio": 20000,
+      "Year":2023,
+      "Marca":"KIA",
+      "Color":"Azul",
+      "ImagenUrl":""
+    }
+  ]
+
+}
